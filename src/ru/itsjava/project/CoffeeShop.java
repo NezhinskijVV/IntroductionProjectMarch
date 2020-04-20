ca50224e08b0d7deed2ac7e97fa2f0210bd17e44
vitaliynezhinskiy <SeekerOfConfessor@gmail.com>
2020-04-20 21:55:41 +0300

    add sorting

--- src/ru/itsjava/project/CoffeeShop.java
+++ src/ru/itsjava/project/CoffeeShop.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class CoffeeShop {
 
     public static void main(String[] args) {
-        String[] items = new String[]{"Frappuccino", "Cappuccino", "Americano", "Cappuccino"};
+        String[] items = new String[]{"Frappuccino", "Espresso", "Americano", "Cappuccino"};
         Scanner consoleShop = new Scanner(System.in);
 
         int menuNum;
@@ -17,10 +17,55 @@ public class CoffeeShop {
                 printItems(items);
             } else if (menuNum == 2) {
                 items = addItem(items, consoleShop);
+            } else if (menuNum == 3) {
+                items = removeItem(items, consoleShop);
             } else if (menuNum == 4) {
                 printContainsItem(items, consoleShop);
+            } else if (menuNum == 5) {
+                sortItems(items);
             }
             System.out.println("Введите номер меню");
+        }
+    }
+
+    private static String[] removeItem(String[] items, Scanner consoleShop) {
+        System.out.println("Введи кофе, который хотите купить");
+        String inputItem = consoleShop.next();
+
+        if (!isContains(items, inputItem)) {
+            System.out.println("Такого кофе нет");
+            return items;
+        }
+
+        String[] resItems = new String[items.length - 1];
+
+        int index;
+        for (index = 0; index < items.length; index++) {
+            if (inputItem.equals(items[index])) {
+                break;
+//            } else if ((index == items.length - 1)) {
+//                return items;
+            } else {
+                resItems[index] = items[index];
+            }
+        }
+
+        for (int i = index; i < resItems.length; i++) {
+            resItems[i] = items[i + 1];
+        }
+
+        return resItems;
+    }
+
+    private static void sortItems(String[] items) {
+        for (int j = 0; j < items.length; j++) {
+            for (int i = 0; i < items.length - 1 - j; i++) {
+                if (items[i].charAt(0) > items[i + 1].charAt(0)) {
+                    String tempName = items[i];
+                    items[i] = items[i + 1];
+                    items[i + 1] = tempName;
+                }
+            }
         }
     }
 
@@ -41,6 +86,14 @@ public class CoffeeShop {
         System.out.println("Введите кофе, который хотите приобрести");
         String inputCoffee = consoleShop.next();
 
+        if (isContains(items, inputCoffee)) {
+            System.out.println("Есть в наличии");
+        } else {
+            System.out.println("Такого кофе нет");
+        }
+    }
+
+    private static boolean isContains(String[] items, String inputCoffee) {
         boolean isContains = false;
         for (String coffee : items) {
             if (inputCoffee.equals(coffee)) {
@@ -48,13 +101,7 @@ public class CoffeeShop {
                 break;
             }
         }
-
-        if (isContains) {
-            System.out.println("Есть в наличии");
-        } else {
-            System.out.println("Такого кофе нет");
-        }
-
+        return isContains;
     }
 
     public static void printItems(String[] items) {
@@ -89,3 +136,4 @@ public class CoffeeShop {
 
 //HW:  Купить кофе
 
+//HW: Сортировка не пузырьком сложностью O(NlogN)
---
+++ src/ru/itsjava/sorting/BubbleSort.java
@@ -0,0 +1,41 @@
+package ru.itsjava.sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+//        int[] numsArray = new int[]{-1, 0, -77, 43, 13, 24, -5};
+//        System.out.println("Изначальный массив:     " + Arrays.toString(numsArray));
+//
+//        for (int j = 0; j < numsArray.length; j++) {
+//            for (int i = 0; i < numsArray.length - 1 - j; i++) {
+//                if (numsArray[i] > numsArray[i + 1]) {
+//                    int tempNum = numsArray[i];
+//                    numsArray[i] = numsArray[i + 1];
+//                    numsArray[i + 1] = tempNum;
+//                }
+//            }
+//        }
+//
+//
+//        System.out.println("Отсортированный массив: " + Arrays.toString(numsArray));
+
+        String[] namesArray = new String[]{"Евгений", "Ярослав", "Георгий", "Дмитрий", "Даниил", "Виталий"};
+//        String[] namesArray = new String[]{"Daniil", "Evgeniy", "Yaroslav", "Georgie", "Dmitriy", "Vitaliy"};
+        System.out.println("Изначальный массив:     " + Arrays.toString(namesArray));
+
+        for (int j = 0; j < namesArray.length; j++) {
+            for (int i = 0; i < namesArray.length - 1 - j; i++) {
+                if (namesArray[i].charAt(0) > namesArray[i + 1].charAt(0)) {
+                    String tempName = namesArray[i];
+                    namesArray[i] = namesArray[i + 1];
+                    namesArray[i + 1] = tempName;
+                }
+            }
+        }
+
+        System.out.println("Отсортированный массив: " + Arrays.toString(namesArray));
+
+    }
+}
